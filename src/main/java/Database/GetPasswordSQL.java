44abc33df4cd0d388b5477277f878b6af56d88ac
queen <Queenmoncler@gmail.com>
2023-10-01 20:45:12 +0300

    add method getPass with button

--- src/main/java/Database/GetPasswordSQL.java
+++ src/main/java/Database/GetPasswordSQL.java
@@ -1,4 +1,26 @@
 package Database;
 
-public class GetPasswordSQL {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class GetPasswordSQL extends ConnectDB {
+    public GetPasswordSQL() throws SQLException {
+    }
+    public static String getPass(String nickname) {
+        String query = "select password from allconfig where nickname = '"+nickname+"';";
+        try(Statement statement = connection.createStatement())  {
+            ResultSet result;
+            result = statement.executeQuery(query);
+            while (result.next()) {
+                return result.getString("password");
+            }
+
+
+        } catch (SQLException e) {
+            System.out.println("Ошибка в getPass");
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
 }
