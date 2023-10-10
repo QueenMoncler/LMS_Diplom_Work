0f62f7630869f2110a3d6be4774a71e901804620
queen <Queenmoncler@gmail.com>
2023-10-10 18:01:20 +0300

    add CommandSQL_Student.java handler sql for Student

---
+++ src/main/java/Database/CommandSQL_Student.java
@@ -0,0 +1,28 @@
+package Database;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CommandSQL_Student extends ConnectDB {
+
+    public CommandSQL_Student() throws SQLException {
+    }
+
+
+    public  String getAmountAllStudent()throws SQLException {
+        String query = "SELECT count(id) from student;";
+        try (Statement statement = connection.createStatement();) {
+            ResultSet result;
+            result = statement.executeQuery(query);
+            while (result.next()) {
+                return result.getString("count");
+            }
+        } catch (SQLException e) {
+            System.out.println("Ошибка в getAmountAllStudent");
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+}
