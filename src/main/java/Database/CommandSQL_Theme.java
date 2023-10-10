961803c3bde6cc753c7bfc8600fcc8eb09eb6cdf
queen <Queenmoncler@gmail.com>
2023-10-10 18:01:05 +0300

    add CommandSQL_Theme.java handler sql for Theme

---
+++ src/main/java/Database/CommandSQL_Theme.java
@@ -0,0 +1,81 @@
+package Database;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CommandSQL_Theme extends ConnectDB {
+
+    public CommandSQL_Theme() throws SQLException {
+
+    }
+
+    public  String getAmountAllTheme()throws SQLException {
+        String query = "SELECT count(id) from theme_ticher;";
+        try (Statement statement = connection.createStatement();) {
+            ResultSet result;
+            result = statement.executeQuery(query);
+            while (result.next()) {
+                return result.getString("count");
+            }
+        } catch (SQLException e) {
+            System.out.println("Ошибка в getAmountAllTheme");
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    public  String getAmountThemeTeacher(String nickname)throws SQLException {
+        String query = "SELECT count(id) from theme_ticher where nickname = '"+nickname+"';";
+        try (Statement statement = connection.createStatement();) {
+            ResultSet result;
+            result = statement.executeQuery(query);
+            while (result.next()) {
+                return result.getString("count");
+            }
+        } catch (SQLException e) {
+            System.out.println("Ошибка в getAmountThemeTeacher");
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    public  String getAmountTaskTeacher(String nickname)throws SQLException{
+        String query = "SELECT count(id) from task_ticher where nickname = '"+nickname+"';";
+        try (Statement statement = connection.createStatement();) {
+            ResultSet result;
+            result = statement.executeQuery(query);
+            while (result.next()) {
+                return result.getString("count");
+            }
+        } catch (SQLException e) {
+            System.out.println("Ошибка в getAmountTaskTeacher");
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+
+    public String addNewThemeTeacher(String nickname, String themeName)throws SQLException{
+        String query = "INSERT INTO theme_ticher(nickname, theme) VALUES('"+nickname+"', '"+themeName+"')";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+                statement.executeUpdate();
+                addNewTheme(themeName);
+                return "Тема добавлена успешно";
+        }
+    }
+
+    public String addNewTheme(String themeName)throws SQLException{
+        String query = "INSERT INTO theme(name) VALUES('"+themeName+"')";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.executeUpdate();
+            return "Тема добавлена успешно";
+        }
+    }
+
+
+}
